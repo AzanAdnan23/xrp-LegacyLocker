8e0c9f70b1ea1ebec92906e943bea620e77c008a
Azan Adnan <azanbutt2311@gmail.com>
2023-10-07 15:12:14 +0500

    hardhat

---
+++ hardhat.config.cjs
@@ -0,0 +1,17 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.21",
+  networks: {
+    evmSidechain: {
+      url: 'https://rpc-evm-sidechain.xrpl.org',
+      chainId: 1440002,
+      gasPrice: 20000000000, // 20 Gwei
+      accounts: [
+       '36be50660cebf0eb609ee223ccd668648553c12867f03832dd74655c2ba1b338'
+      ]
+    }
+  },
+  defaultNetwork: 'evmSidechain'
+};
