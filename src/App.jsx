5c0035efad7f7e35a1c0ff1e9e3d2fad65419be5
Azan Adnan <azanbutt2311@gmail.com>
2023-10-09 23:33:23 +0500

    metamask

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import WalletCard from './WalletCard';
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <p> ABC KITHA GAI C </p>
+        <WalletCard/>
     </>
   )
 }
