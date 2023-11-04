5133983c034ced883f4fb7f1d86e26a5bad734d1
Azan Adnan <azanbutt2311@gmail.com>
2023-11-04 20:11:35 +0500

    logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,12 +2,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import WalletCard from "../components/WalletCard";
+import logo from '/src/assets/logo.png'; 
 
 const NavBar = ({ onConnect }) => (
   <nav className="bg-black text-white p-6  font-semibold font-sans">
-    <ul className="flex space-x-4 justify-between ">
+    <ul className="flex space-x-4 justify-between items-center">
       <li>
-        <Link to="/">Home</Link>
+        <Link to="/">
+          <img src={logo} alt="Home" className="h-10 w-auto"/> 
+        </Link>
       </li>
       <li>
         <Link to="/userdashboard">Legacy Dashboard</Link>
